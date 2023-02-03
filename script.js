14633fbc1205c50211cbd4912a18da822e719be0
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-02-03 10:55:18 +0100

    version 2

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 const searchField = document.getElementById('searchInput');
 const container = document.getElementById('container');
+const baseURL = 'http://gateway.marvel.com/v1/public/characters?apikey=069e5f498025d779169cc4f23fe0e56b&hash=b019d95f3ca74d6d390a341b0013f98b&ts=258'
+const header = document.getElementById('header');
+
 let query;
 
-searchField.addEventListener('input', getQuery)
+searchField.addEventListener('input', function () {
+	getQuery();
+	console.log(query);
+	fetch(baseURL + '&nameStartsWith=' + query)
+		.then(response => response.json())
+		.then(characters => {
+
+			reset();
+
+			if (characters.data.results.length) {
+				characters.data.results.forEach(character => {
+					container.innerHTML += `
+				<div class="character">
+				 <img src="${character.thumbnail.path}.${character.thumbnail.extension}">
+				 <div class="imgHeader">${character.name}<span><i class="fa-solid fa-star"></i></span>
+				 </div>
+			`
+				});
+			} else {
+				header.innerHTML += '<p>No hero with such name.Please, try another one.</p>';
+
+			}
+
+		});
+});
 
 function getQuery() {
-	query = this.value;
+	query = searchField.value;
+	console.log(query);
 }
 
-
-const ts = "258";
-const hash = "b019d95f3ca74d6d390a341b0013f98b"
-
-
-fetch('http://gateway.marvel.com/v1/public/characters?apikey=069e5f498025d779169cc4f23fe0e56b&ts=' + ts + '&hash=' + hash)
-	.then(response => response.json())
-	.then(characters => {
-		console.log(characters);
-		characters.data.results.forEach(character => {
-			container.innerHTML +=`
-				${character.id} ${character.name} <br>
-			`
-		});;
-	}
-
-	)
+function reset() {
+	container.innerHTML = '';
+	
+}
